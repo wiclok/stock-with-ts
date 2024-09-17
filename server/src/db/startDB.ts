79d55ece043bcd6b5e55f47e18865a15b8f99a16
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-09-17 19:25:09 -0300

    execution of the relationship function

--- server/src/db/startDB.ts
+++ server/src/db/startDB.ts
@@ -1,11 +1,13 @@
 import { seedRoles } from '../seed/role.sedd';
 import { sequelize } from './connection';
+import { executeRelations } from './relations';
 
 export const startDB = async () => {
   sequelize
     .authenticate()
     .then(() => {
       sequelize.sync({ force: false });
+      executeRelations();
       seedRoles();
       console.log('Conectado a la base de datos');
 
