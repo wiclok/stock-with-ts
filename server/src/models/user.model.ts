73690361b091d855c0f429edef036f81e3032a29
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-09-17 18:14:01 -0300

    role property update

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,13 +1,15 @@
 import { Model, DataTypes } from 'sequelize';
 import { sequelize } from '../db/connection';
+import RoleModel from './role.model';
 import { User } from './interface/user.interface';
+import BranchModel from './branch.model';
 
 class UserModel extends Model<User> implements User {
   public id!: number;
   public userName!: string;
   public email!: string;
   public password!: string;
-  public role!: 'admin' | 'user';
+  public roleId!: number;
   public state!: boolean;
 }
 
@@ -31,12 +33,31 @@ UserModel.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    role: {
-      type: DataTypes.ENUM('admin', 'user'),
-      defaultValue: 'user',
+    roleId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: RoleModel,
+        key: 'id',
+      },
+    },
+    state: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
     },
   },
-  { sequelize, tableName: 'Users' },
+  { sequelize, tableName: 'Users' }
 );
+
+// Establecemos el hook para asignar el rol por defecto
+UserModel.beforeCreate(async (user) => {
+  if (!user.roleId) {
+    const defaultRole = await RoleModel.findOne({ where: { roleName: 'auditor' } });
+    if (defaultRole) {
+      user.roleId = defaultRole.id;
+    }
+  }
+});
+
+
 
 export default UserModel;
